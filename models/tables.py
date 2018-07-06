79e797f8ea2263bdda2988642f742b3841288648
Dustboonies <dtfeldsine@gmail.com>
2018-07-06 13:32:10 -0700

    Added DB Tables for each type of part as well as main pc

--- models/tables.py
+++ models/tables.py
@@ -7,7 +7,50 @@
 # There is an implicit 'id integer autoincrement' field
 # Consult manual for more options, validators, etc.
 
-
+db.define_table('pc',
+                Field('user_email', default=auth.user.email if auth.user else None),
+                )
+                
+db.define_table('cpu',
+                Field('cpu_name', 'text'),
+                Field('cpu_speed', 'text'),
+                Field('cpu_chipset', 'text'),
+                )
+                
+db.define_table('gpu',
+                Field('gpu_name', 'text'),
+                Field('gpu_speed', 'text'),
+                Field('gpu_chipset', 'text'),
+                )
+                
+db.define_table('mem',
+                Field('mem_name', 'text'),
+                Field('mem_speed', 'text'),
+                Field('mem_chipset', 'text'),
+                )
+                
+db.define_table('psu',
+                Field('psu_name', 'text'),
+                Field('psu_wattage', 'text'),
+                Field('psu_rating', 'text'),
+                )
+                
+db.define_table('mobo',
+                Field('mobo_name', 'text'),
+                Field('mobo_size', 'text'),
+                Field('mobo_chipset', 'text'),
+                )
+                
+db.define_table('hdd',
+                Field('hdd_name', 'text'),
+                Field('hdd_type', 'text'),
+                Field('hdd_size', 'text'),
+                )
+                
+db.define_table('tower',
+                Field('tower_name', 'text'),
+                Field('tower_size', 'text'),
+                )
 
 
 # after defining tables, uncomment below to enable auditing
