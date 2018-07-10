257dbda046a285e5101a148457790035173ffc02
Kevin Velasquez <ksvelasq@ucsc.edu>
2018-07-10 14:13:58 -0700

    changed the name of the function names to end with _func

--- static/js/default_index.js
+++ static/js/default_index.js
@@ -14,44 +14,44 @@ var app = function() {
     };
     var enumerate = function(v) { var k=0; return v.map(function(e) {e._idx = k++;});};
     
-    self.cpu_list = function () {
+    self.cpu_list_func = function () {
        console.log("in cpu_list");
        $.getJSON(cpu_list_url, function (data) {
            self.vue.cpu_list = data.cpu_list;
            enumerate(self.vue.search_list);
        })
     };
-    self.gpu_list = function () {
+    self.gpu_list_func = function () {
        console.log("in gpu_list");
        $.getJSON(gpu_list_url, function (data) {
            self.vue.gpu_list = data.gpu_list;
        })
     };
-    self.mem_list = function () {
+    self.mem_list_func = function () {
        console.log("in mem_list");
        $.getJSON(mem_list_url, function (data) {
            self.vue.mem_list = data.mem_list;
        })
     };
-    self.mobo_list = function () {
+    self.mobo_list_func = function () {
        console.log("in mobo_list");
        $.getJSON(mobo_list_url, function (data) {
            self.vue.mobo_list = data.mobo_list;
        })
     };
-    self.hdd_list = function () {
+    self.hdd_list_func = function () {
        console.log("in hdd_list");
        $.getJSON(hdd_list_url, function (data) {
            self.vue.hdd_list = data.hdd_list;
        })
     };
-    self.psu_list = function () {
+    self.psu_list_func = function () {
        console.log("in psu_list");
        $.getJSON(psu_list_url, function (data) {
            self.vue.psu_list = data.psu_list;
        })
     };
-    self.case_list = function () {
+    self.case_list_func = function () {
        console.log("in case_list");
        $.getJSON(case_list_url, function (data) {
            self.vue.case_list = data.case_list;
