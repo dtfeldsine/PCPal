ce5dc6d279b2aaf76d018f1f980b603070758de3
Anthony Lin <anthonylin@Anthonys-MacBook-Pro-2.local>
2018-07-12 14:22:36 -0700

    changes to see list of cpus on site

--- mysite/PCPal/views.py
+++ mysite/PCPal/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from django.template import loader
+
+from .models import CPU
 # Create your views here.
 
-def index(request):
-    return HttpResponse("welcum 2 pcpale")
-
+def cpu_index(request):
+    latest_cpu_list = CPU.objects.order_by('-cpu_name')[:100]
+    template = loader.get_template('PCPal/index.html')
+    context = {
+        'latest_cpu_list': latest_cpu_list,
+}
+    return HttpResponse(template.render(context, request))
 
