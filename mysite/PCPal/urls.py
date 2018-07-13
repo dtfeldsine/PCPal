af1a7f4208cacd632e6d3e557661656042dac108
Anthony Lin <anthonylin@Anthonys-MacBook-Pro-2.local>
2018-07-12 19:22:15 -0700

    rest of tabs should work now

--- mysite/PCPal/urls.py
+++ mysite/PCPal/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.cpu_index, name = 'cpu_index'),
+  #  path('', views.gpu_index, name = 'gpu_index'),
 ]
+
 
 
--- mysite/PCPal/views.py
+++ mysite/PCPal/views.py
@@ -3,45 +3,25 @@ from django.http import HttpResponse
 from django.template import loader
 
 from .models import CPU
+from .models import GPU
+from .models import RAM
+from .models import MOBO
+from .models import HDD
+
 # Create your views here.
 
 def cpu_index(request):
     latest_cpu_list = CPU.objects.order_by('-cpu_name')[:100]
-    template = loader.get_template('PCPal/index.html')
+    latest_gpu_list = GPU.objects.order_by('-gpu_name')[:100]
+    latest_ram_list = RAM.objects.order_by('-ram_name')[:100]
+    latest_mobo_list = MOBO.objects.order_by('-mobo_name')[:100]
+    latest_hdd_list = HDD.objects.order_by('-hdd_name')[:100]
     context = {
         'latest_cpu_list': latest_cpu_list,
+        'latest_gpu_list': latest_gpu_list, 
+        'latest_ram_list': latest_ram_list, 
+        'latest_mobo_list': latest_mobo_list, 
+        'latest_hdd_list': latest_hdd_list, 
     }
-    return HttpResponse(template.render(context, request))
-    
-def gpu_index(request):
-    latest_gpu_list = GPU.objects.order_by('-gpu_name')[:100]
-    template = loader.get_template('PCPal/index.html')
-    context = {
-        'latest_gpu_list': latest_gpu_list,
-    }
-    return HttpResponse(template.render(context, request))
-    
-def ram_index(request):
-    latest_ram_list = RAM.objects.order_by('-ram_name')[:100]
-    template = loader.get_template('PCPal/index.html')
-    context = {
-        'latest_ram_list': latest_ram_list,
-    }
-    return HttpResponse(template.render(context, request))
-    
-def mobo_index(request):
-    latest_mobo_list = MOBO.objects.order_by('-mobo_name')[:100]
-    template = loader.get_template('PCPal/index.html')
-    context = {
-        'latest_mobo_list': latest_mobo_list,
-    }
-    return HttpResponse(template.render(context, request))
-    
-def hdd_index(request):
-    latest_hdd_list = HDD.objects.order_by('-hdd_name')[:100]
-    template = loader.get_template('PCPal/index.html')
-    context = {
-        'latest_hdd_list': latest_hdd_list,
-    }
-    return HttpResponse(template.render(context, request))
-
+    return render(request, 'PCPal/index.html', context)
+ 
