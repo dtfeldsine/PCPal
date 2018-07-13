e2deaa3fa80a603f8ba5b4600fdbc4ab0de0f71c
Dustboonies <dtfeldsine@gmail.com>
2018-07-12 18:16:18 -0700

    Added lists for GPU, HDD, MOBO, and RAM

--- mysite/PCPal/views.py
+++ mysite/PCPal/views.py
@@ -10,6 +10,38 @@ def cpu_index(request):
     template = loader.get_template('PCPal/index.html')
     context = {
         'latest_cpu_list': latest_cpu_list,
-}
+    }
+    return HttpResponse(template.render(context, request))
+    
+def gpu_index(request):
+    latest_gpu_list = GPU.objects.order_by('-gpu_name')[:100]
+    template = loader.get_template('PCPal/index.html')
+    context = {
+        'latest_gpu_list': latest_gpu_list,
+    }
+    return HttpResponse(template.render(context, request))
+    
+def ram_index(request):
+    latest_ram_list = RAM.objects.order_by('-ram_name')[:100]
+    template = loader.get_template('PCPal/index.html')
+    context = {
+        'latest_ram_list': latest_ram_list,
+    }
+    return HttpResponse(template.render(context, request))
+    
+def mobo_index(request):
+    latest_mobo_list = MOBO.objects.order_by('-mobo_name')[:100]
+    template = loader.get_template('PCPal/index.html')
+    context = {
+        'latest_mobo_list': latest_mobo_list,
+    }
+    return HttpResponse(template.render(context, request))
+    
+def hdd_index(request):
+    latest_hdd_list = HDD.objects.order_by('-hdd_name')[:100]
+    template = loader.get_template('PCPal/index.html')
+    context = {
+        'latest_hdd_list': latest_hdd_list,
+    }
     return HttpResponse(template.render(context, request))
 
